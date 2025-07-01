c8b7fb16e662b8d826fb7c0fa0dc0cab75b4368f
Gino Campra <gino@gcampra.com.br>
2025-07-01 16:06:27 -0300

    fix: include valorMora in total calculation for boleto value

--- src/Boleto/AbstractBoleto.php
+++ src/Boleto/AbstractBoleto.php
@@ -1455,7 +1455,7 @@ abstract class AbstractBoleto implements BoletoContract
             return (float) $default;
         }
 
-        return (float) ($this->getValor() + $this->getValorMulta());
+        return (float) ($this->getValor() + $this->getValorMulta() + $this->getValorMora());
     }    
 
     /**
